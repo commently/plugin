ea091c0260588d90002aff3889553d7431b723ac
Yuriy Gromchenko <gromchen@gmail.com>
2021-11-23 23:35:43 +0200

    Execute script in a page

--- popup.js
+++ popup.js
@@ -3,3 +3,15 @@ const changeColorElement = document.getElementById('changeColor')
 chrome.storage.sync.get('color', ({ color }) => {
   changeColorElement.style.backgroundColor = color
 })
+
+changeColorElement.addEventListener('click', async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+
+  chrome.scripting.executeScript({ target: { tabId: tab.id }, function: setPageBackgroundColor })
+})
+
+function setPageBackgroundColor() {
+  chrome.storage.sync.get('color', ({ color }) => {
+    document.body.style.backgroundColor = color
+  })
+}
