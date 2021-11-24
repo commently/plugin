faf13cfe8b5191b2a7e4eecaf8034c628a21e4e9
Yuriy Gromchenko <gromchen@gmail.com>
2021-11-24 19:17:47 +0200

    Remove popup

--- background.js
+++ background.js
@@ -2,5 +2,14 @@ const color = '#3aa757'
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color })
-  console.log('Default background color set to %cgreen', `color: ${color}`);
 })
+
+chrome.action.onClicked.addListener(tab => {
+  chrome.scripting.executeScript({ target: { tabId: tab.id }, function: setPageBackgroundColor })
+})
+
+function setPageBackgroundColor() {
+  chrome.storage.sync.get('color', ({ color }) => {
+    document.body.style.backgroundColor = color
+  })
+}
