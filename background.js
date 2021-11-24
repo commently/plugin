2bb79eb98eb5f84d710c895c1867104f3df7c163
Yuriy Gromchenko <gromchen@gmail.com>
2021-11-24 19:30:53 +0200

    Enable only for https pages

--- background.js
+++ background.js
@@ -2,6 +2,21 @@ const color = '#3aa757'
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color })
+
+  chrome.action.disable()
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    const rule = {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { schemes: ['https'] }
+        })
+      ],
+      actions: [new chrome.declarativeContent.ShowAction()],
+    }
+
+    chrome.declarativeContent.onPageChanged.addRules([rule])
+  })
 })
 
 chrome.action.onClicked.addListener(tab => {
