5b4ec4cabff3b99fcedb36acf8aa3912e102ed7f
Yuriy Gromchenko <gromchen@gmail.com>
2021-12-01 22:30:08 +0100

    Remove tsdx and options

--- extension/app/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { HTMLAttributes, ReactChild } from 'react';
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-    /** custom content, defaults to 'the snozzberries taste like snozzberries' */
-    children?: ReactChild;
-}
