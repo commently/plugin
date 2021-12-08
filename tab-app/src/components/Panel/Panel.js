0e4428881862f35a0a4d578a25c34778e569881a
Yuriy Gromchenko <gromchen@gmail.com>
2021-12-09 00:14:45 +0300

    Add buttons to the panel

--- tab-app/src/components/Panel/Panel.js
+++ tab-app/src/components/Panel/Panel.js
@@ -1,10 +1,16 @@
+import classNames from 'classnames'
 import TextareaAutosize from 'react-textarea-autosize'
-import { root, inputField } from './Panel.module.css'
+import { root, inputField, buttonsRow, button_default, button_primary, button_secondary } from './Panel.module.css'
 
 function Panel() {
   return (
     <div className={root}>
       <TextareaAutosize className={inputField} placeholder="Add a comment" cacheMeasurements maxRows={10} />
+
+      <div className={buttonsRow}>
+        <button className={classNames(button_default, button_secondary)}>Cancel</button>  
+        <button className={classNames(button_default, button_primary)}>Post</button>  
+      </div>
     </div>
   )
 }
