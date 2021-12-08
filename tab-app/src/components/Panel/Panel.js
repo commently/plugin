e184c567ac04d3a6eb110fefa17693a798fcef6e
Yuriy Gromchenko <gromchen@gmail.com>
2021-12-08 23:34:44 +0300

    Add textarea to the panel

--- tab-app/src/components/Panel/Panel.js
+++ tab-app/src/components/Panel/Panel.js
@@ -1,5 +1,11 @@
+import { root, inputField } from './Panel.module.css'
+
 function Panel() {
-  return null
+  return (
+    <div className={root}>
+      <textarea className={inputField} placeholder="Add a comment" />
+    </div>
+  )
 }
 
 export default Panel
---
+++ tab-app/src/components/Panel/Panel.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Panel from './Panel'
+
+export default { component: Panel };
+
+const Template = (args) => <Panel {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
